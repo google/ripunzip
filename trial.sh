8e78ed5d435432837c9f5f1f2ca6efd7b91de8df
adetaylor <ade@hohum.me.uk>
2022-11-21 23:21:46 +0000

    Add time trial

---
+++ trial.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+ZIPFILE=~/Downloads/mac-release_asan-mac-release-1008971.zip
+MYDIR=$(pwd)
+
+mkdir /tmp/testa
+pushd /tmp/testa
+time sh -c "unzip -l $ZIPFILE > /dev/null"
+popd
+
+rm -Rf /tmp/testa
+
+mkdir /tmp/testb
+pushd /tmp/testb
+time sh -c "$MYDIR/target/release/runzip $ZIPFILE > /dev/null"
+popd
+
+rm -Rf /tmp/testb
