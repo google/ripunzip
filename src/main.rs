1facf074d84c2ef74575681190dc2dca4a087f91
adetaylor <ade@hohum.me.uk>
2022-11-21 23:19:19 +0000

    Lists OK

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,8 @@ struct Args {
 #[derive(Clone)]
 struct CloneableFile {
     file: Arc<Mutex<File>>,
-    pos: Option<u64>,
+    pos: u64,
+    // TODO determine and store this once instead of per cloneable file
     file_length: Option<u64>,
 }
 
@@ -30,7 +31,7 @@ impl<'a> CloneableFile {
     fn new(file: File) -> Self {
         Self {
             file: Arc::new(Mutex::new(file)),
-            pos: None,
+            pos: 0u64,
             file_length: None,
         }
     }
@@ -42,6 +43,7 @@ impl CloneableFile {
             Some(file_length) => file_length,
             None => {
                 let len = self.file.lock().unwrap().metadata().unwrap().len();
+                println!("File length determined to be {}", len);
                 self.file_length = Some(len);
                 len
             }
@@ -52,33 +54,39 @@ impl CloneableFile {
 impl Read for CloneableFile {
     fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
         let mut underlying_file = self.file.lock().expect("Unable to get underlying file");
-        if let Some(seek_from) = self.pos {
-            underlying_file.seek(SeekFrom::Start(seek_from))?;
+        // TODO share an object which knows current position to avoid unnecessary
+        // seeks
+        underlying_file.seek(SeekFrom::Start(self.pos))?;
+        let read_result = underlying_file.read(buf);
+        if let Ok(bytes_read) =  read_result {
+            // TODO, once stabilised, use checked_add_signed
+            self.pos += bytes_read as u64;
         }
-        underlying_file.read(buf)
+        read_result
     }
 }
 
 impl Seek for CloneableFile {
     fn seek(&mut self, pos: SeekFrom) -> std::io::Result<u64> {
-        let prior_pos = self.pos.unwrap_or_default();
         let new_pos = match pos {
             SeekFrom::Start(pos) => pos,
             SeekFrom::End(offset_from_end) => {
                 let file_len = self.ascertain_file_length();
                 // TODO, once stabilised, use checked_add_signed
-                file_len - (offset_from_end as u64)
+                file_len - (-offset_from_end as u64)
             }
             // TODO, once stabilised, use checked_add_signed
             SeekFrom::Current(offset_from_pos) => {
-                if offset_from_pos > 0 {
-                    prior_pos + (offset_from_pos as u64)
+                if offset_from_pos == 0 {
+                    self.pos
+                } else if offset_from_pos > 0 {
+                    self.pos + (offset_from_pos as u64)
                 } else {
-                    prior_pos - ((-offset_from_pos) as u64)
+                    self.pos - ((-offset_from_pos) as u64)
                 }
             }
         };
-        self.pos = Some(new_pos);
+        self.pos = new_pos;
         Ok(new_pos)
     }
 }
@@ -89,6 +97,7 @@ fn main() -> Result<()> {
     let zipfile = CloneableFile::new(zipfile);
     let zip = zip::ZipArchive::new(zipfile)?;
     let file_count = zip.len();
+    println!("Zip has {} files", file_count);
     (0..file_count).into_par_iter().for_each(|i| {
         let mut myzip = zip.clone();
         let file = myzip.by_index(i).expect("Unable to get file from zip");
