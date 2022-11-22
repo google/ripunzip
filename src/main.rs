a07a702b5275724efcf035d8b4b230623a1deeb0
Adrian Taylor <adetaylor@chromium.org>
2022-11-22 21:04:51 +0000

    Better error handling.

--- src/main.rs
+++ src/main.rs
@@ -15,13 +15,16 @@
 mod cloneable_seekable_reader;
 
 use std::{
+    borrow::Cow,
     fs::{create_dir_all, File},
-    path::PathBuf,
+    io::ErrorKind,
+    path::{Path, PathBuf},
 };
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use clap::{Parser, Subcommand};
 use rayon::prelude::*;
+use zip::read::ZipFile;
 
 use crate::cloneable_seekable_reader::CloneableSeekableReader;
 
@@ -77,26 +80,57 @@ fn unzip_file(zipfile: File, output_directory: &Option<PathBuf>) -> Result<()> {
     let zip = zip::ZipArchive::new(zipfile)?;
     let file_count = zip.len();
     println!("Zip has {} files", file_count);
-    (0..file_count).into_par_iter().for_each(|i| {
-        let mut myzip = zip.clone();
-        let mut file = myzip.by_index(i).expect("Unable to get file from zip");
-        let name = file.name();
-        println!("Filename: {}", name);
-        if name.ends_with('/') {
-            println!("Skipping, directory");
-        } else {
-            let out_file = match output_directory {
-                Some(output_directory) => output_directory.join(file.name()),
-                None => PathBuf::from(file.name()),
-            };
-            if let Some(parent) = out_file.parent() {
-                create_dir_all(parent).unwrap_or_else(|err| {
-                    panic!("Unable to create parent directories for {}: {}", name, err)
-                });
-            }
-            let mut out_file = File::create(out_file).unwrap();
-            std::io::copy(&mut file, &mut out_file).unwrap();
+    let errors: Vec<_> = (0..file_count)
+        .into_par_iter()
+        .map(|i| extract_file(zip.clone(), i, output_directory))
+        .filter_map(Result::err)
+        .collect();
+    // Output any errors we found on any file
+    for error in &errors {
+        eprintln!("Error: {}", error)
+    }
+    // Return the first error code, if any.
+    errors.into_iter().next().map(Result::Err).unwrap_or(Ok(()))
+}
+
+/// Extracts a file from a zip file, attaching diagnostics to any errors where
+/// possible.
+fn extract_file(
+    mut myzip: zip::ZipArchive<CloneableSeekableReader<File>>,
+    i: usize,
+    output_directory: &Option<PathBuf>,
+) -> Result<()> {
+    let file = myzip.by_index(i)?;
+    let name = file
+        .enclosed_name()
+        .map(Path::to_string_lossy)
+        .unwrap_or_else(|| Cow::Borrowed("<unprintable>"))
+        .to_string();
+    extract_file_inner(file, output_directory)
+        .with_context(|| format!("Failed to extract {}", name))
+}
+
+/// Extracts a file from a zip file.
+fn extract_file_inner(mut file: ZipFile, output_directory: &Option<PathBuf>) -> Result<()> {
+    let name = file
+        .enclosed_name()
+        .ok_or_else(|| std::io::Error::new(ErrorKind::Unsupported, "path not safe to extract"))?;
+    let name = name.to_path_buf();
+    // let name_for_error = name.clone();
+    // let add_context = || format!("Failed to extract {}", name_for_error);
+    if name.is_dir() {
+        println!("Skipping directory {}", name.display());
+    } else {
+        println!("Extracting: {}", name.display());
+        let out_file = match output_directory {
+            Some(output_directory) => output_directory.join(file.name()),
+            None => PathBuf::from(file.name()),
+        };
+        if let Some(parent) = out_file.parent() {
+            create_dir_all(parent)?;
         }
-    });
+        let mut out_file = File::create(out_file)?;
+        std::io::copy(&mut file, &mut out_file)?;
+    }
     Ok(())
 }
